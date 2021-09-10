7d4e0a6d9b1f3173c7773b83520d2dfdaf68fbfe
Harishsnd <saundankarharish17@gmail.com>
2021-09-10 09:46:07 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('.food-slider').slick({
+    autoplay:true,
+     slidesToShow:3,
+     slidesToScroll:1,
+     prevArrow:".prev-btn",
+     nextArrow:".next-btn",
+     responsive:[
+           {
+              breakpoint:801,
+              settings:{
+                slidesToShow:2,
+              }
+
+           }
+      ]
+   
+   });
+
+  
+  $('.nav-trigger').click(function(){
+      $('.site-content-wrapper').toggleClass('scaled');
+  })
+
+
+});
